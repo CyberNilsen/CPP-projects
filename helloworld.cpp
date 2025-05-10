8056d6abd0b7b5195848e740fe9aebf8e293424b
Barnegraver <andreasnilsen07@hotmail.com>
2025-05-10 15:16:17 +0200

    Changed and added new line.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,7 @@
 #include <iostream> // funksjoner for input og output.
 
 int main() {
-    std::cout << "Hello world!";
+    std::cout << "Hello world!" << '\n';
+        std::cout << "Test" << '\n';
     return 0;
 }
